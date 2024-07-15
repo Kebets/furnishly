5b8d627abe2f2a5be3cd06f193c272059e1161e1
Kebets Alexander <kebetsav@gmail.com>
2024-07-15 22:23:26 +0300

    added fields, Lombok annotations and Javadoc

--- src/main/java/com/ast/furnishly/entities/Manufacturer.java
+++ src/main/java/com/ast/furnishly/entities/Manufacturer.java
@@ -1,4 +1,20 @@
 package com.ast.furnishly.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The manufacturer of Furniture
+ *
+ * Relation:
+ * Many to One: Furniture -> Manufacturer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Manufacturer {
+    private Long id;
+    private String name;
+    private String country;
 }
