8707ff7ce0b18c2f10f9a63785e23c79279fdb63
Kebets Alexander <kebetsav@gmail.com>
2024-07-15 22:23:31 +0300

    added fields, Lombok annotations and Javadoc

--- src/main/java/com/ast/furnishly/entities/Furniture.java
+++ src/main/java/com/ast/furnishly/entities/Furniture.java
@@ -1,4 +1,26 @@
 package com.ast.furnishly.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Furniture entity
+ *
+ * Relation:
+ * Many To One: Furniture -> Type
+ * Many To One: Furniture -> Manufacturer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Furniture {
+    private int id;
+    private Type type;
+    private String name;
+    private Manufacturer manufacturer;
+    private BigDecimal price;
+    private int quantity;
 }
