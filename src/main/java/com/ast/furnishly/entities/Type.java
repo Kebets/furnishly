9e850e33bd92e715698743f30ffa28c087318ece
Kebets Alexander <kebetsav@gmail.com>
2024-07-15 22:23:19 +0300

    added fields, Lombok annotations and Javadoc

--- src/main/java/com/ast/furnishly/entities/Type.java
+++ src/main/java/com/ast/furnishly/entities/Type.java
@@ -1,4 +1,19 @@
 package com.ast.furnishly.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The type of Furniture
+ *
+ * Relation:
+ * Many to One: Furniture -> Type
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Type {
+    private Long id;
+    private String name;
 }
