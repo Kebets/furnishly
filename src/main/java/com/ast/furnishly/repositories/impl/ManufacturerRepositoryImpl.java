1d7d34a23e5b1d9eb8d82f4da967aac676a84c24
Kebets Alexander <kebetsav@gmail.com>
2024-07-17 22:05:35 +0300

    added findAll() and createManufacturer methods

--- src/main/java/com/ast/furnishly/repositories/impl/ManufacturerRepositoryImpl.java
+++ src/main/java/com/ast/furnishly/repositories/impl/ManufacturerRepositoryImpl.java
@@ -1,12 +1,30 @@
 package com.ast.furnishly.repositories.impl;
 
 import com.ast.furnishly.entities.Manufacturer;
+import com.ast.furnishly.repositories.ConnectionManager;
 import com.ast.furnishly.repositories.ManufacturerRepository;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class ManufacturerRepositoryImpl implements ManufacturerRepository {
+    private static final String FIND_ALL_MANUFACTURERS = "SELECT * FROM manufacturer";
+
+    private static ManufacturerRepository instance;
+    private final ConnectionManager connectionManager = ConnectionManagerImpl.getConnectionManager();
+
+    public static ManufacturerRepository getInstance() {
+        if (instance == null) {
+            instance = new ManufacturerRepositoryImpl();
+        }
+        return instance;
+    }
+
     @Override
     public Manufacturer save(Manufacturer manufacturer) {
         return null;
@@ -29,11 +47,28 @@ public class ManufacturerRepositoryImpl implements ManufacturerRepository {
 
     @Override
     public List<Manufacturer> findAll() {
-        return List.of();
+        List<Manufacturer> manufacturers = new ArrayList<>();
+        try(Connection connection = connectionManager.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(FIND_ALL_MANUFACTURERS);
+            while(resultSet.next()){
+                manufacturers.add(createManufacturer(resultSet));
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return manufacturers;
     }
 
     @Override
-    public boolean exitsById(Long id) {
+    public boolean existsById(Long id) {
         return false;
+    }
+
+    public Manufacturer createManufacturer(ResultSet resultSet) throws SQLException {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(resultSet.getLong(1));
+        manufacturer.setName(resultSet.getString(2));
+        return manufacturer;
     }
 }
