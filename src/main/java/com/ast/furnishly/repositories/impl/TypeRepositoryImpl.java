62d0c70c4d7eae7c3565b1133ac71f2d7a2a6da2
Kebets Alexander <kebetsav@gmail.com>
2024-07-17 22:05:18 +0300

    added findAll() and createType methods

--- src/main/java/com/ast/furnishly/repositories/impl/TypeRepositoryImpl.java
+++ src/main/java/com/ast/furnishly/repositories/impl/TypeRepositoryImpl.java
@@ -1,12 +1,29 @@
 package com.ast.furnishly.repositories.impl;
 
 import com.ast.furnishly.entities.Type;
+import com.ast.furnishly.repositories.ConnectionManager;
 import com.ast.furnishly.repositories.TypeRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class TypeRepositoryImpl implements TypeRepository {
+    private static String FIND_ALL_TYPES = "SELECT * FROM type";
+
+    private static TypeRepository instance;
+    private final ConnectionManager connectionManager = ConnectionManagerImpl.getConnectionManager();
+
+    public static TypeRepository getInstance() {
+        if (instance == null) {
+            instance = new TypeRepositoryImpl();
+        }
+        return instance;
+    }
 
     @Override
     public Type save(Type type) {
@@ -30,11 +47,28 @@ public class TypeRepositoryImpl implements TypeRepository {
 
     @Override
     public List<Type> findAll() {
-        return List.of();
+        List<Type> types = new ArrayList<>();
+        try(Connection connection = connectionManager.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_ALL_TYPES);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                types.add(createType(resultSet));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();//TODO add logging
+        }
+        return types;
     }
 
     @Override
-    public boolean exitsById(Long id) {
+    public boolean existsById(Long id) {
         return false;
+    }
+
+    private Type createType(ResultSet resultSet) throws SQLException {
+        Type type = new Type();
+        type.setId(resultSet.getLong("id"));
+        type.setName(resultSet.getString("name"));
+        return type;
     }
 }
