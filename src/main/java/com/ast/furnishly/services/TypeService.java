29efa562833cd712b030a275152a37a3372784aa
Kebets Alexander <kebetsav@gmail.com>
2024-07-18 22:07:11 +0300

    added getTypeById and findAll() methods

--- src/main/java/com/ast/furnishly/services/TypeService.java
+++ src/main/java/com/ast/furnishly/services/TypeService.java
@@ -1,4 +1,10 @@
 package com.ast.furnishly.services;
 
+import com.ast.furnishly.dto.TypeDto;
+
+import java.util.List;
+
 public interface TypeService {
+    TypeDto getTypeById(Long id);
+    List<TypeDto> findAll();
 }
