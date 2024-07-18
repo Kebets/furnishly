d40aaa92efd62d2cc5d99835981d7c76fa7f0e75
Kebets Alexander <kebetsav@gmail.com>
2024-07-18 22:07:28 +0300

    created TypeServiceImpl class

---
+++ src/main/java/com/ast/furnishly/services/impl/TypeServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ast.furnishly.services.impl;
+
+import com.ast.furnishly.dto.TypeDto;
+import com.ast.furnishly.services.TypeService;
+
+import java.util.List;
+
+public class TypeServiceImpl implements TypeService {
+    @Override
+    public TypeDto getTypeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TypeDto> findAll() {
+        return List.of();
+    }
+}
