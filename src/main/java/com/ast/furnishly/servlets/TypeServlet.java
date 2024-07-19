451aa5e866627cdc63de6ee63f177a4f44494e9e
Kebets Alexander <kebetsav@gmail.com>
2024-07-19 22:24:57 +0300

    created TypeServlet

---
+++ src/main/java/com/ast/furnishly/servlets/TypeServlet.java
@@ -0,0 +1,71 @@
+package com.ast.furnishly.servlets;
+
+import com.ast.furnishly.services.TypeService;
+import com.ast.furnishly.services.impl.TypeServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/type/*"})
+//@WebServlet("/type")
+public class TypeServlet extends HttpServlet {
+    private final TypeService typeService;
+    private final ObjectMapper objectMapper;
+
+    public TypeServlet(){
+        this.typeService = TypeServiceImpl.getInstance();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    private static void setJsonHeader(HttpServletResponse response) {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+    }
+
+    private static String getJson(HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader postData = request.getReader();
+        String line;
+        while ((line = postData.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello");
+//        setJsonHeader(resp);
+
+        /*String responseAnswer = "";
+        try {
+            String[] pathPart = req.getPathInfo().split("/");
+            if ("all".equals(pathPart[1])) {
+                List<TypeDto> typeDtoList = typeService.findAll();
+                resp.setStatus(HttpServletResponse.SC_OK);
+                responseAnswer = objectMapper.writeValueAsString(typeDtoList);
+                responseAnswer = "Hi!";
+            } else {
+                Long typeId = Long.parseLong(pathPart[1]);
+                TypeDto typeDto = typeService.findById(typeId);
+                resp.setStatus(HttpServletResponse.SC_OK);
+                responseAnswer = objectMapper.writeValueAsString(typeDto);
+                responseAnswer = "Hi!";
+            }
+        } catch (NotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseAnswer = e.getMessage();
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            responseAnswer = "Bad request.";
+        }
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write(responseAnswer);
+        printWriter.flush();*/
+    }
+}
