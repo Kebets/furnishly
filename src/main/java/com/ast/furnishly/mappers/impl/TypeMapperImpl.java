5ef617c8411db4220d0d51b60606b6fdc446b4b0
Kebets Alexander <kebetsav@gmail.com>
2024-07-19 22:22:26 +0300

    added getInstance method

--- src/main/java/com/ast/furnishly/mappers/impl/TypeMapperImpl.java
+++ src/main/java/com/ast/furnishly/mappers/impl/TypeMapperImpl.java
@@ -7,6 +7,15 @@ import com.ast.furnishly.mappers.TypeMapper;
 import java.util.List;
 
 public class TypeMapperImpl implements TypeMapper {
+    private static TypeMapper typeMapper;
+
+    public static synchronized TypeMapper getInstance(){
+        if (typeMapper == null){
+            typeMapper = new TypeMapperImpl();
+        }
+        return typeMapper;
+    }
+
     @Override
     public TypeDto map(Type type) {
         return new TypeDto(type.getName());
