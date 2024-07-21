7876c03cfa1e5304d4c96fa557bf81e57ecd998e
Kebets Alexander <kebetsav@gmail.com>
2024-07-21 22:47:43 +0300

    added Type map(TypeDto) and creected some code

--- src/main/java/com/ast/furnishly/mappers/impl/TypeMapperImpl.java
+++ src/main/java/com/ast/furnishly/mappers/impl/TypeMapperImpl.java
@@ -5,6 +5,7 @@ import com.ast.furnishly.entities.Type;
 import com.ast.furnishly.mappers.TypeMapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TypeMapperImpl implements TypeMapper {
     private static TypeMapper typeMapper;
@@ -18,11 +19,16 @@ public class TypeMapperImpl implements TypeMapper {
 
     @Override
     public TypeDto map(Type type) {
-        return new TypeDto(type.getName());
+        return new TypeDto(type.getId(), type.getName());
+    }
+
+    @Override
+    public Type map(TypeDto typeDto) {
+        return new Type(typeDto.getId(), typeDto.getName());
     }
 
     @Override
     public List<TypeDto> map(List<Type> typeList) {
-        return (List<TypeDto>) typeList.stream().map(Type::getName);
+        return typeList.stream().map(typeMapper::map).collect(Collectors.toList());
     }
 }
