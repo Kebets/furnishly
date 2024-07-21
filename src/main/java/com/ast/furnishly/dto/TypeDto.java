a352e798b54618532c73b5b8416e1058e7c71864
Kebets Alexander <kebetsav@gmail.com>
2024-07-21 22:46:00 +0300

    changed from @NoArgsConstructor to @Data and deleted extra code

--- src/main/java/com/ast/furnishly/dto/TypeDto.java
+++ src/main/java/com/ast/furnishly/dto/TypeDto.java
@@ -1,20 +1,16 @@
 package com.ast.furnishly.dto;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@Data
 public class TypeDto {
+    private Long id;
     private String name;
 
-    public TypeDto(String name) {
+    public TypeDto(Long id, String name) {
+        this.id = id;
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
