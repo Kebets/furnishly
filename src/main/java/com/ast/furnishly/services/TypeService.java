41ba2183395931772e7350d8bab6cfcce7dac0c8
Kebets Alexander <kebetsav@gmail.com>
2024-07-21 22:50:00 +0300

    added delete, save and update abstract methods

--- src/main/java/com/ast/furnishly/services/TypeService.java
+++ src/main/java/com/ast/furnishly/services/TypeService.java
@@ -1,10 +1,14 @@
 package com.ast.furnishly.services;
 
 import com.ast.furnishly.dto.TypeDto;
+import com.ast.furnishly.exceptions.NotFoundException;
 
 import java.util.List;
 
 public interface TypeService {
     TypeDto findById(Long id);
     List<TypeDto> findAll();
+    boolean delete(Long id) throws NotFoundException;
+    TypeDto save(TypeDto typeDto);
+    void update(Long id, TypeDto typeDto) throws NotFoundException;
 }
